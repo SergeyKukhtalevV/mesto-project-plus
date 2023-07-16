9693e41aaa8808f6f42e2153f2b97572f08ed518
Sergey Kukhtalev <s.kuhtalev@arktur.su>
2023-07-16 14:14:27 +0300

    feature: added controller and router for user

---
+++ src/controllers/users.ts
@@ -0,0 +1,19 @@
+import { Request, Response } from "express";
+import User from "../models/user";
+
+export const getUsers = (req: Request, res: Response) => User.find({})
+  .then((users) => res.send(users))
+  .catch(() => res.status(500).send({ message: "Произошла ошибка получения информации о пользователях" }));
+
+export const getUserById = (req: Request, res: Response) => User.findById(req.params.userId)
+  .then((users) => res.send(users))
+  .catch(() => res.status(500).send({ message: "Произошла ошибка. ID пользователя не найден" }));
+
+export const createUser = (req: Request, res: Response) => {
+  const { name, about, avatar } = req.body;
+  return User.create({ name, about, avatar })
+    .then((user) => res.send({
+      name, about, avatar, _id: user._id,
+    }))
+    .catch(() => res.status(500).send({ message: "Произошла ошибка при создании пользователя." }));
+};
---
+++ src/routes/users.ts
@@ -0,0 +1,11 @@
+import { Router } from "express";
+
+import { getUsers, getUserById, createUser } from "../controllers/users";
+
+const router = Router();
+
+router.get("/", getUsers);
+router.get("/:userId", getUserById);
+router.post("/", createUser);
+
+export default router;
