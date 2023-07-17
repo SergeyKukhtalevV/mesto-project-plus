257f48c1caf9c9c8f90459170eb6048593a02ed4
Sergey Kukhtalev <s.kuhtalev@arktur.su>
2023-07-17 08:53:29 +0300

    feature: added model for cards

---
+++ src/models/card.ts
@@ -0,0 +1,42 @@
+import mongoose, { Schema, Types } from "mongoose";
+
+interface ICard {
+  name: string;
+  link: string;
+  avatar: string;
+  owner: Schema.Types.ObjectId;
+  likes: Schema.Types.ObjectId[];
+  createdAt: Date;
+}
+
+const cardSchema = new Schema<ICard>(
+  {
+    name: {
+      type: String,
+      minlength: 2,
+      maxLength: 30,
+      required: true,
+    },
+    link: {
+      type: String,
+      required: true,
+      match: /^http?:\/\/.+/,
+    },
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: "user",
+      required: true,
+    },
+    likes: {
+      type: [Types.ObjectId],
+      default: [],
+      ref: "user",
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  { versionKey: false },
+);
+export default mongoose.model<ICard>("card", cardSchema);
