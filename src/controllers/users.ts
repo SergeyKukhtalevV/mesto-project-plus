e01c77e9e81b8299a887d78ae4affa05c87812c1
Sergey Kukhtalev <s.kuhtalev@arktur.su>
2023-07-19 11:07:02 +0300

    feature: added patch request for user's info and avatar

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -1,19 +1,100 @@
 import { Request, Response } from "express";
 import User from "../models/user";
+import { ExpandedRequest } from "./cards";
 
 export const getUsers = (req: Request, res: Response) => User.find({})
   .then((users) => res.send(users))
-  .catch(() => res.status(500).send({ message: "Произошла ошибка получения информации о пользователях" }));
+  .catch(() => res.status(500)
+    .send({ message: "Произошла ошибка получения информации о пользователях" }));
 
 export const getUserById = (req: Request, res: Response) => User.findById(req.params.userId)
   .then((users) => res.send(users))
-  .catch(() => res.status(500).send({ message: "Произошла ошибка. ID пользователя не найден" }));
+  .catch(() => res.status(500)
+    .send({ message: "Произошла ошибка. ID пользователя не найден" }));
 
 export const createUser = (req: Request, res: Response) => {
-  const { name, about, avatar } = req.body;
-  User.create({ name, about, avatar })
+  const {
+    name,
+    about,
+    avatar,
+  } = req.body;
+  User.create({
+    name,
+    about,
+    avatar,
+  })
     .then((user) => res.send({
-      name, about, avatar, _id: user._id,
+      name,
+      about,
+      avatar,
+      _id: user._id,
     }))
-    .catch((err) => res.status(500).send({ message: `Произошла ошибка при создании пользователя. ${err}` }));
+    .catch((err) => res.status(500)
+      .send({ message: `Произошла ошибка при создании пользователя. ${err}` }));
+};
+
+export const patchAboutUser = (req: ExpandedRequest, res: Response) => {
+  const {
+    name,
+    about,
+    avatar,
+  } = req.body;
+  const userId = req.user?._id;
+  User.findOneAndUpdate(
+    { userId },
+    {
+      name,
+      about,
+      avatar,
+    },
+    {
+      new: true,
+      runValidators: true,
+    },
+  )
+    .then((updateUser) => {
+      if (!updateUser) {
+        throw new Error("Ошибка при обновлении данных о пользователе");
+      }
+      res.send(updateUser);
+    })
+    .catch((err) => {
+      if (err.name === "ValidationError") {
+        res
+          .status(400)
+          .send({ message: "Ошибка валидации" });
+      }
+      res
+        .status(500)
+        .send({ message: err.message });
+    });
+};
+
+export const patchAvatarUser = (req: ExpandedRequest, res: Response) => {
+  const { avatar } = req.body;
+  const userId = req.user?._id;
+  User.findOneAndUpdate(
+    { userId },
+    { avatar },
+    {
+      new: true,
+      runValidators: true,
+    },
+  )
+    .then((updateUser) => {
+      if (!updateUser) {
+        throw new Error("Ошибка при обновлении аватара пользователя");
+      }
+      res.send(updateUser);
+    })
+    .catch((err) => {
+      if (err.name === "ValidationError") {
+        res
+          .status(400)
+          .send({ message: "Ошибка валидации" });
+      }
+      res
+        .status(500)
+        .send({ message: err.message });
+    });
 };
--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,11 +1,19 @@
 import { Router } from "express";
 
-import { getUsers, getUserById, createUser } from "../controllers/users";
+import {
+  getUsers,
+  getUserById,
+  createUser,
+  patchAboutUser,
+  patchAvatarUser,
+} from "../controllers/users";
 
 const usersRouter = Router();
 
 usersRouter.get("/", getUsers);
 usersRouter.get("/:userId", getUserById);
 usersRouter.post("/", createUser);
+usersRouter.patch("/me", patchAboutUser);
+usersRouter.patch("/me/avatar", patchAvatarUser);
 
 export default usersRouter;
