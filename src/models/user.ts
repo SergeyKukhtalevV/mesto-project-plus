181163ce03c32a15a52ae3138e18e9a4008dcf36
Sergey Kukhtalev <s.kuhtalev@arktur.su>
2023-07-26 15:48:15 +0300

    feature: added email and password to userSchema

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,12 +1,22 @@
 import mongoose from "mongoose";
+import validator from "validator";
+import bcrypt from "bcrypt";
 
 interface IUser {
   name: string;
   about: string;
   avatar: string;
+  email: string;
+  password: string;
 }
 
-const userSchema = new mongoose.Schema<IUser>(
+interface UserModel extends mongoose.Model<IUser> {
+  // eslint-disable-next-line no-unused-vars
+  findUserByCredentials: (email: string, password: string) =>
+    Promise<mongoose.Document<unknown, any, IUser>>;
+}
+
+const userSchema = new mongoose.Schema<IUser, UserModel>(
   {
     name: {
       type: String,
@@ -25,8 +35,37 @@ const userSchema = new mongoose.Schema<IUser>(
       required: true,
       match: /^https?:\/\/.+/,
     },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      validate: {
+        validator: (email: string) => validator.isEmail(email),
+      },
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 8,
+    },
   },
   { versionKey: false },
 );
+userSchema.static("findUserByCredentials", function findUserByCredentials(email: string, password: string) {
+  return this.findOne({ email })
+    .then((user) => {
+      if (!user) {
+        return Promise.reject(new Error("Неправильные почта или пароль"));
+      }
 
-export default mongoose.model<IUser>("user", userSchema);
+      return bcrypt.compare(password, user.password)
+        .then((matched: boolean) => {
+          if (!matched) {
+            return Promise.reject(new Error("Неправильные почта или пароль"));
+          }
+
+          return user; // теперь user доступен
+        });
+    });
+});
+export default mongoose.model<IUser, UserModel>("user", userSchema);
