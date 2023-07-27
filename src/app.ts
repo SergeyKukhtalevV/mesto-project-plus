398d81a643d990cc5348c3ecdac7fbf4be1d12a8
Sergey Kukhtalev <s.kuhtalev@arktur.su>
2023-07-27 13:56:49 +0300

    feature: added requestLogger and errorLogger

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,7 @@ import router from "./routes/index";
 import errorMiddleware from "./middleware/errorMiddleware";
 import { createUser, login } from "./controllers/users";
 import auth from "./middleware/auth";
+import { requestLogger, errorLogger } from "./middleware/logger";
 
 const { PORT = 3000 } = process.env;
 
@@ -11,10 +12,13 @@ const { PORT = 3000 } = process.env;
 const app = express();
 mongoose.connect("mongodb://0.0.0.0:27017/mestodb");
 app.use(express.json());
+app.use(requestLogger); // подключаем логер запросов
 app.post("/signin", login);
 app.post("/signup", createUser);
 app.use(auth as express.NextFunction);
-app.use(router, errorMiddleware);
+app.use(router);
+app.use(errorLogger); // подключаем логер ошибок
+app.use(errorMiddleware);
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
 });
